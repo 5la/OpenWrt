33f4fa7884738e9cacf7fd0ceb2fc348fa948aab
5la <86772550+5la@users.noreply.github.com>
2022-07-17 00:15:13 +0800

    Update diy-part2.sh
    del luci-app-ttnode

--- armv8/mini/diy-part2.sh
+++ armv8/mini/diy-part2.sh
@@ -31,7 +31,6 @@ git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-git clone  https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/luci-app-netdata
 svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman package/luci-app-dockerman
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-smartdns package/luci-app-smartdns
